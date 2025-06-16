60c7a948e04d877d39143018c28ab99084e76711
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-06-16 23:19:40 +0530

    # List of symptoms

--- Disease_predict.py
+++ Disease_predict.py
@@ -4,3 +4,18 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import joblib
 
+# List of symptoms
+symptoms = [
+    "fever",
+    "headache",
+    "nausea",
+    "vomiting",
+    "fatigue",
+    "joint_pain",
+    "skin_rash",
+    "cough",
+    "weight_loss",
+    "yellow_eyes"
+]
+
+
